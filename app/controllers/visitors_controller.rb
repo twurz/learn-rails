29f04dfd3d13e26b811ee172d51531eb2b22e101
twurz <travis.wurzer@yahoo.com>
2014-03-25 16:34:17 -0700

    fixed owner.rb file

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,8 +1,9 @@
 class VisitorsController < ApplicationController
-  def new
 
+  def new
+    Rails.logger.debug 'DEBUG: entering new method'
     @owner = Owner.new
-    render 'visitors/new'
+    Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
   end
 
 end
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -6,6 +6,7 @@ class Owner
     birthdate = Date.new(1990, 12, 22)
   end
   def countdown
+    Rails.logger.debug 'DEBUG: entering Owner countdown method'
     today = Date.today
     birthday = Date.new(today.year, birthdate.month, birthdate.day)
     if birthday > today
